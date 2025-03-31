024460243856126b6559867d24944e684123b8e9
yangqiang <124517196@qq.com>
2025-03-31 22:26:51 +0800

    递归及快速排序

---
+++ ChapterOne/Quicksort.py
@@ -0,0 +1,50 @@
+# def sum(arr):
+#     total = 0
+#     for i in arr:
+#         total +=i
+#     return  total
+#
+# print(sum([1,2,3,4,5]))
+
+
+#练习4.1：请编写前述sum函数的代码
+
+# def sum_total(arr):
+#     length = len(arr)
+#     if length==1:
+#         return arr[0]
+#     else:
+#         total = arr.pop(0)+sum_total(arr)
+#     return total
+#
+# print(sum_total([1,2,3,4,5]))
+
+#练习4.2：编写一个递归函数来计算列表包含的元素数
+# def count_total(arr):
+#
+#     if not arr:
+#         return 0
+#     else:
+#         arr.pop()
+#         total= 1 + count_total(arr)
+#
+#     return total
+#
+# print(count_total([1,2,3,4,5]))
+
+# def max_num(arr):
+#     length = len(arr)
+#     max_number = arr[0]
+#     if  length==1:
+#         return max_number
+#     else:
+#         if arr[0]>arr[1]:
+#             arr.pop(1)
+#         else:
+#             arr.pop(0)
+#         return max_num(arr) #此处一定要加上return，不然函数返回None;
+#
+# print(max_num([1, 2, 3, 4, 7]))
+
+
+
---
+++ ChapterOne/Recursion.py
@@ -0,0 +1,40 @@
+# def look_for_key(main_box):
+#     pile = main_box.make_a_pile_to_look_through()
+#     while pile is not empty:
+#         box = pile.grap_a_box()
+#         for item in box:
+#            if item.is_a_box:
+#                pile.append(item)
+#            elif item.is_a_key:
+#                print("found the key!")
+#
+# def look_for_key():
+#     for item in box:
+#         if item.is_a_box:
+#             look_for_key(item)
+#         elif item.is_a_key:
+#             print("found the key!")
+#
+# def greet(name):
+#     print("hello,"+name+"!")
+#     greet2(name)
+#     print("getting ready to say goodbye...")
+#     bye()
+#
+# def greet2(name):
+#     print("how are you,"+name+"?")
+#
+# def bye():
+#     print("ok bye!")
+#
+# greet("maggie")
+
+
+# def fact(x):
+#     if x == 1:  #基线条件
+#         return 1
+#     else:
+#         return x * fact(x-1)    #递归条件
+#
+# print(fact(5))
+
