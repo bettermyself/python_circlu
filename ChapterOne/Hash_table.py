b7103480869d43ab729544a96be4b96c4a31cf4c
Yangqiang <1124517196@qq.com>
2025-04-01 14:09:12 +0800

    散列表

---
+++ ChapterOne/Hash_table.py
@@ -0,0 +1,25 @@
+# vote= {}
+#
+# def check_coter(name):
+#     if vote.get(name):
+#         print("kick them out!")
+#     else:
+#         vote[name]=True
+#         print("let them vote!")
+#
+# check_coter("tom")
+# check_coter("mike")
+# check_coter("mike")
+
+
+# chche = {}
+#
+# def get_page(url):
+#     if chche.get(url):
+#         return chche[url]
+#     else:
+#         date = get_data_from_server(url)
+#         chche[url] = date
+#         return date
+
+
